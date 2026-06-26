90fd2ca8d2265c20911bc389f2a356139871c45a
AvalosRuben <rubenavalosmtz@gmail.com>
2026-06-26 13:12:45 -0600

    feat(Database): connection with variables stored on .env

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host = "localhost"
+	port = 5432
+)
+
 func main() {
+
+	err:= godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbName)
+
+	db,err := sql.Open("postgres",psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	 fmt.Println("Database connected :p")
 	
 }
