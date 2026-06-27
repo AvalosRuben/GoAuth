ef355588b9d502be7b69d1a77ffa4c1b9e44eaba
AvalosRuben <rubenavalosmtz@gmail.com>
2026-06-27 17:46:57 -0600

    feat(login): Created the ComparePasswords function to check passwords

--- controllers/userController.go
+++ controllers/userController.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/AvalosRuben/GoAuth/models"
 	"github.com/gin-gonic/gin"
@@ -47,6 +48,41 @@ func HashPassword(password string, p *params)(hash string, err error){
 
 }
 
+func HashPasswordWithSalt(password string, p *params, salt []byte)(hash string, err error){
+
+	rawHash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+	hashString := hex.EncodeToString(rawHash)
+	saltString := hex.EncodeToString(salt)
+
+	hash = hashString + "$" + saltString
+
+    return hash, nil
+
+}
+
+func ComparePasswords(password string, hashedPassword string, p *params, c *gin.Context)(isEqual bool){
+	hashAndSalt := strings.Split(hashedPassword, "$")
+	saltString := hashAndSalt[1]
+	rawSalt, err := hex.DecodeString(saltString)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Papu error checking password": err.Error()})
+	}
+
+	rawHashedInputPassword, err := HashPasswordWithSalt(password, p, []byte(rawSalt))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Papu error checking password": err.Error()})
+	}
+
+	inputHashAndSalt := strings.Split(rawHashedInputPassword, "$")
+
+	if hashAndSalt[0] == inputHashAndSalt[0] {
+		return true
+	}
+
+	return false
+	
+}
+
 func Signup(db *gorm.DB)gin.HandlerFunc{
 
 	p := &params{
@@ -80,8 +116,35 @@ func Signup(db *gorm.DB)gin.HandlerFunc{
 }
 
 func Login(db *gorm.DB)gin.HandlerFunc{
+	p := &params{
+        memory:      64 * 1024,
+        iterations:  3,
+        parallelism: 2,
+        saltLength:  16,
+        keyLength:   32,
+    }
+	
 	return func (c *gin.Context){
+		var user models.User
+		var inputUser models.User
+		if err := c.BindJSON(&inputUser);err!=nil{
+			c.JSON(http.StatusBadRequest, gin.H{"papu error": err.Error()})
+			return
+		}
+
+		result := db.Where("mail = ?",inputUser.Mail).First(&user)
+		log.Println("result: ",result)
+
+		equalPasswords := ComparePasswords(inputUser.HashedPassword, user.HashedPassword,p, c)
+
+		if equalPasswords {
+			log.Println("Equal")
+		} else{
+			log.Println("Not Equal")
+		}
 		
+		c.JSON(http.StatusOK,inputUser)
+
 	}
 }
 
